75f09ca54d968cf8f1da9e0d78c6f8bdae63e7f0
GAsplund <gasplund2@gmail.com>
2022-09-16 16:00:17 +0200

    Add GPS location

--- get-a-room/src/main/java/com/chalmers/group30/views/LeafletMap.java
+++ get-a-room/src/main/java/com/chalmers/group30/views/LeafletMap.java
@@ -2,12 +2,16 @@ package com.chalmers.group30.views;
 
 import com.google.gson.JsonObject;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.dependency.JavaScript;
 import software.xdev.vaadin.maps.leaflet.flow.LMap;
 
+@JavaScript("https://opengeo.tech/maps/leaflet-gps/dist/leaflet-gps.min.js")
 public class LeafletMap extends LMap {
-    public void setGeoJson(JsonObject obj) {
-        String setObj = "var geoJsonString = " + obj.toString() + ";";
-        this.getElement().executeJs(setObj + "L.geoJSON(geoJsonString).addTo(this.map);");
+    public LeafletMap() {
+        super();
+        this.getElement().executeJs("this.map.addControl( new L.Control.Gps({autoActive:true, setView:true}) );");
     }
-
+    public void setGeoJson(JsonObject obj) {
+        this.getElement().executeJs("L.geoJSON(" + obj.toString() + ").addTo(this.map);");
+    }
 }
