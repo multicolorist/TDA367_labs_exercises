fa61e030c490652ec0dbb1352a8d0bd31b4e5c6d
GAsplund <gasplund2@gmail.com>
2022-09-16 19:15:00 +0200

    Add route display, route maneuver deserialisation

--- get-a-room/src/main/java/com/chalmers/group30/views/LeafletMap.java
+++ get-a-room/src/main/java/com/chalmers/group30/views/LeafletMap.java
@@ -1,5 +1,7 @@
 package com.chalmers.group30.views;
 
+import com.chalmers.group30.models.objects.Location;
+import com.chalmers.group30.models.objects.Route;
 import com.google.gson.JsonObject;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.dependency.JavaScript;
@@ -13,5 +15,23 @@ public class LeafletMap extends LMap {
     }
     public void setGeoJson(JsonObject obj) {
         this.getElement().executeJs("L.geoJSON(" + obj.toString() + ").addTo(this.map);");
+    }
+
+    public void drawRoute(Route r) {
+        // Generate JS array of locations
+        StringBuilder locationArrayString = new StringBuilder("[");
+        for(Location point : r.maneuvers()){
+            locationArrayString.append("[").append(point.latitude()).append(",").append(point.longitude()).append("],");
+        }
+        // Remove final stray comma
+        locationArrayString.deleteCharAt(locationArrayString.length()-1);
+        locationArrayString.append("]");
+
+        // Add a line representing the route
+        this.getElement().executeJs("L.polyline(" + locationArrayString.toString() + ", {color: 'blue'}).addTo(this.map);");
+
+        // Display destination marker
+        Location finalLoc = r.maneuvers().get(r.maneuvers().size() - 1);
+        this.getElement().executeJs("L.marker(["+finalLoc.latitude()+", "+finalLoc.longitude()+"]).addTo(this.map);");
     }
 }
