0b471bbf46466a92bd289bf69c2524e54905e43e
multicolorist <multicolorist@users.noreply.github.com>
2022-09-21 13:23:15 +0200

    Updated main view: cleaner and better UI, more consistent code style

--- get-a-room/src/main/java/com/chalmers/group30/views/main/MainView.java
+++ get-a-room/src/main/java/com/chalmers/group30/views/main/MainView.java
@@ -1,16 +1,22 @@
 package com.chalmers.group30.views.main;
 
-import com.chalmers.group30.controllers.DarkLightModeButtonController;
-import com.chalmers.group30.controllers.FilterButtonController;
+import com.chalmers.group30.controllers.*;
+import com.chalmers.group30.models.objects.Location;
 import com.chalmers.group30.models.objects.Room;
+import com.chalmers.group30.views.components.buttons.BookButton;
 import com.chalmers.group30.views.components.buttons.DarkLightModeButton;
 import com.chalmers.group30.views.components.buttons.FilterButton;
+import com.chalmers.group30.views.components.buttons.showOnMapButton;
+import com.chalmers.group30.views.components.controls.DateTimeControls;
+import com.chalmers.group30.views.components.controls.IntegerUnlabeledStepper;
+import com.chalmers.group30.views.components.displays.DistancePill;
 import com.vaadin.flow.component.*;
+import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.details.Details;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -23,42 +29,100 @@ import java.util.List;
 
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @PageTitle("GetARoom")
 @Route(value = "")
-public class MainView extends Main implements HasComponents, HasStyle {
+// public class MainView extends Main implements HasComponents, HasStyle {
+public class MainView extends AppLayout implements HasComponents, HasStyle {
 
     // UI
     DarkLightModeButtonController darkLightModeButtonController;
     DarkLightModeButton darkLightModeButton;
     FilterButtonController filterButtonController;
     FilterButton filterButton;
+    RoomListController roomListController;
 
-    private final ComponentRenderer<Component, Room> roomCardRenderer = new ComponentRenderer<>(room -> {
-        HorizontalLayout cardLayout = new HorizontalLayout();
-        cardLayout.setMargin(true);
+    // Class rendering the room list
+    private final ComponentRenderer<Component, Room> roomListEntryRenderer = new ComponentRenderer<>(room -> {
+        // Buttons
+        Button bookButton = new BookButton(room.uuid());
+        bookButton.addClickListener(BookButtonController.getListener());
+        Button showMapButton = new showOnMapButton(room.uuid());
+        showMapButton.addClickListener(ShowOnMapButtonController.getListener());
 
-        Avatar avatar = new Avatar(room.name(), "");
-        avatar.setHeight("64px");
-        avatar.setWidth("64px");
+        // Layouts
+        HorizontalLayout listEntryLayout = new HorizontalLayout();
+        listEntryLayout.addClassNames(
+                // LumoUtility.Display.BLOCK
+                LumoUtility.Width.FULL
+        );
 
-        VerticalLayout infoLayout = new VerticalLayout();
-        infoLayout.setSpacing(false);
-        infoLayout.setPadding(false);
-        infoLayout.getElement().appendChild(ElementFactory.createStrong(room.name()));
-        infoLayout.add(new Div(new Text(room.building())));
-        infoLayout.add(new Div(new Text("Latitude: " + room.location().latitude())));
-        infoLayout.add(new Div(new Text("Longitude: " + room.location().longitude())));
+        // Image, avatar or distance to display to the left in the card
+        Location userLocation = new Location(57.696484034673915, 11.975264592149706); // TODO: Change this to the user's location from a controller
+        int distanceToRoom = (int) Math.round(roomListController.getBirdsDistance(userLocation, room.location()));// TODO: Change this to the user's location from a controller
+        DistancePill distanceIndicator = new DistancePill(distanceToRoom);
+        // Avatar distanceIndicator = new Avatar(distanceToRoom + " meters to this room from your location");
+        // distanceIndicator.setAbbreviation(distanceToRoom + "m");
+        // distanceIndicator.setHeight("64px");
+        // distanceIndicator.setWidth("64px");
+        distanceIndicator.addClassNames(
+                LumoUtility.AlignSelf.CENTER
+        );
 
-        VerticalLayout detailsLayout = new VerticalLayout();
-        detailsLayout.setSpacing(false);
-        detailsLayout.setPadding(false);
-        detailsLayout.add(new Div(new Text(room.uuid().toString())));
-        infoLayout.add(new Details("Details", detailsLayout));
+        // Top left part of the card, seen both folded and unfolded
+        HorizontalLayout topLayout = new HorizontalLayout();
+        topLayout.addClassNames(
+                LumoUtility.Width.FULL
+                );
 
-        cardLayout.add(avatar, infoLayout);
-        return cardLayout;
+        VerticalLayout topLayoutLeft = new VerticalLayout();
+        topLayoutLeft.getElement().appendChild(ElementFactory.createStrong(room.name()));
+        topLayoutLeft.add(new Div(new Text(distanceToRoom + " meters away")));
+
+        // VerticalLayout topSpacer = new VerticalLayout();
+
+        // Top right part of the card, seen both folded and unfolded
+        VerticalLayout topLayoutRight = new VerticalLayout();
+        topLayoutRight.add(new Div(new Text(room.building())));
+        topLayoutRight.add(new Div(new Text("Floor " + room.floor())));
+
+        // topLayout.add(topLayoutLeft, topSpacer, topLayoutRight);
+        topLayout.add(topLayoutLeft, topLayoutRight);
+
+        // Bottom part of the card, seen only when unfolded
+        VerticalLayout bottomLayout = new VerticalLayout();
+        // HorizontalLayout textRow = new HorizontalLayout();
+        // textRow.addClassNames(
+        //         LumoUtility.AlignSelf.CENTER
+        // );
+        // textRow.add(new Div(new Text(room.streetAddress())));
+        HorizontalLayout buttonRow = new HorizontalLayout(); // Lower row for buttons
+
+        buttonRow.add(showMapButton, bookButton);
+        buttonRow.addClassNames(
+                // LumoUtility.Display.FLEX,
+                LumoUtility.AlignSelf.CENTER
+        );
+        // bottomLayout.add(textRow,buttonRow);
+        bottomLayout.add(buttonRow);
+
+        // Add the content to the card
+        listEntryLayout.add(distanceIndicator, topLayout);
+
+        // Make a foldable panel of the entry
+        // https://vaadin.com/api/platform/23.2.1/com/vaadin/flow/component/accordion/AccordionPanel.html or
+        // https://vaadin.com/docs/latest/components/details
+        // TODO: Figure out how to add shadow DOM CSS of summary-content part to width: 100%
+        Details foldablePanel = new Details(listEntryLayout, bottomLayout);
+        // foldablePanel.addThemeVariants(DetailsVariant.FILLED);
+        foldablePanel.addClassNames(
+                LumoUtility.Padding.MEDIUM,
+                LumoUtility.BoxShadow.SMALL // https://vaadin.com/docs/latest/styling/lumo/utility-classes/#box-shadow
+        );
+        return foldablePanel;
+        // return listEntryLayout;
     });
 
 
@@ -67,66 +131,128 @@ public class MainView extends Main implements HasComponents, HasStyle {
             DarkLightModeButtonController darkLightModeButtonController,
             DarkLightModeButton darkLightModeButton,
             FilterButtonController filterButtonController,
-            FilterButton filterButton) throws IOException {
+            FilterButton filterButton,
+            RoomListController roomListController) throws IOException {
 
         // Init fields from dependency injection
         this.darkLightModeButtonController = darkLightModeButtonController;
         this.darkLightModeButton = darkLightModeButton;
         this.filterButtonController = filterButtonController;
         this.filterButton = filterButton;
+        this.roomListController = roomListController;
 
         // Add additional styling for main view
         // TODO: Is this best-practice in Vaadin?
-        addClassNames("main-view", "max-w-screen-lg", "mx-auto", "pb-l", "px-l");
+        // addClassNames("main-view", "max-w-screen-lg", "mx-auto", "pb-l", "px-l");
+        addClassNames(
+                // "main-view",
+                // LumoUtility.MaxWidth.SCREEN_XLARGE,
+                LumoUtility.Margin.Horizontal.AUTO,
+                LumoUtility.Padding.Bottom.LARGE,
+                LumoUtility.Padding.Horizontal.LARGE
+        );
 
         // Get rooms
         // Data
-        List<Room> rooms = darkLightModeButtonController.getRooms();
+        List<Room> rooms = roomListController.getRooms();
 
         // Init filter and dark/light mode button
         filterButton.addClickListener(filterButtonController.getListener());
         darkLightModeButton.addClickListener(darkLightModeButtonController.getListener());
 
         // Container
-        VerticalLayout container = new VerticalLayout();
-        container.setAlignItems(FlexComponent.Alignment.STRETCH);
-        container.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
+        // VerticalLayout container = new VerticalLayout();
+        // container.setAlignItems(FlexComponent.Alignment.STRETCH);
+        // container.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
 
         // Header
-        H2 header = new H2("GetARoom");
-        header.addClassNames("mb-0", "mt-xl", "text-3xl");
+        H2 title = new H2("GetARoom");
+        title.addClassNames(
+                LumoUtility.Margin.Bottom.SMALL,
+                LumoUtility.Margin.Top.SMALL,
+                LumoUtility.FontSize.XXXLARGE
+        );
         HorizontalLayout headerContainer = new HorizontalLayout();
-        headerContainer.setAlignItems(FlexComponent.Alignment.CENTER);
-        headerContainer.setAlignSelf(FlexComponent.Alignment.END,
-                this.filterButton,
-                this.darkLightModeButton
+        headerContainer.addClassNames(
+                LumoUtility.Width.FULL,
+                LumoUtility.Padding.Horizontal.MEDIUM,
+                LumoUtility.JustifyContent.BETWEEN,
+                LumoUtility.AlignItems.CENTER,
+                LumoUtility.AlignSelf.CENTER,
+                LumoUtility.Background.BASE
         );
         headerContainer.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
         headerContainer.add(
                 this.filterButton,
-                header,
+                title,
                 this.darkLightModeButton
         );
 
-        // Room card list
-        VerticalLayout listContainer = new VerticalLayout();
-        VirtualList<Room> roomCardsList = new VirtualList<>();
-        roomCardsList.setItems(rooms);
-        roomCardsList.setRenderer(roomCardRenderer);
-        roomCardsList.setSizeFull();
-        listContainer.add(roomCardsList);
-
-
-        // Add to main VerticalLayout container - do we need this?
-        container.add(headerContainer);
-
-        // Add to main view
-        add(
-                container,
-                roomCardsList
+        // Query container
+        Div queryContainer = new Div();
+        queryContainer.addClassNames(
+                LumoUtility.Padding.Horizontal.MEDIUM,
+                LumoUtility.JustifyContent.CENTER,
+                LumoUtility.AlignItems.CENTER,
+                LumoUtility.AlignSelf.CENTER,
+                LumoUtility.Background.BASE,
+                LumoUtility.FontSize.LARGE,
+                LumoUtility.FontWeight.SEMIBOLD
+        );
+        HorizontalLayout roomQueryRow = new HorizontalLayout();
+        roomQueryRow.add(
+                new Div(new Text("Rooms for ")),
+                new IntegerUnlabeledStepper(1, 12, 4),
+                new Div(new Text(" people from "))
+        );
+        roomQueryRow.addClassNames(
+                LumoUtility.Padding.Horizontal.MEDIUM,
+                LumoUtility.JustifyContent.CENTER,
+                LumoUtility.AlignItems.CENTER,
+                LumoUtility.AlignSelf.CENTER,
+                LumoUtility.Background.BASE,
+                LumoUtility.FontSize.LARGE,
+                LumoUtility.FontWeight.SEMIBOLD
         );
 
-        setSizeFull();
+        queryContainer.add(
+                roomQueryRow,
+                new DateTimeControls()
+        );
+
+        // Navbar
+        VerticalLayout navbarContainer = new VerticalLayout(); // To keep elements vertically ordered
+        navbarContainer.addClassNames(
+                LumoUtility.Width.FULL,
+                LumoUtility.Padding.Horizontal.MEDIUM,
+                LumoUtility.JustifyContent.BETWEEN, // horizontal
+                LumoUtility.AlignItems.CENTER, // vertical
+                LumoUtility.AlignSelf.CENTER,  // vertical
+                LumoUtility.Background.BASE
+        );
+        navbarContainer.add(headerContainer, queryContainer);
+
+        addToNavbar(navbarContainer);
+
+
+        // Room list
+        VirtualList<Room> roomList = new VirtualList<>();
+        roomList.addClassNames(
+                LumoUtility.Padding.Horizontal.MEDIUM,
+                LumoUtility.Padding.Top.MEDIUM,
+                LumoUtility.Padding.Bottom.XLARGE
+        );
+        roomList.setItems(rooms);
+        roomList.setRenderer(roomListEntryRenderer);
+        roomList.setHeight("85%");
+
+
+        // Main view composition (header in navbar which is already a child)
+        add(
+                roomList
+        );
+
+        // setSizeFull();
     }
 
 }
