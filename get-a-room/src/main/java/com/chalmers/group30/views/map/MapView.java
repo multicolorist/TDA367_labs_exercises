89d515d2a3b4f9550114ac04777385cdac04334f
GAsplund <gasplund2@gmail.com>
2022-09-23 18:46:29 +0200

    Add basic MapLibre component with hardcoded data

--- get-a-room/src/main/java/com/chalmers/group30/views/map/MapView.java
+++ get-a-room/src/main/java/com/chalmers/group30/views/map/MapView.java
@@ -4,6 +4,7 @@ import com.chalmers.group30.models.ChalmersMapsAPI;
 import com.chalmers.group30.models.objects.Location;
 import com.chalmers.group30.models.objects.Route;
 import com.chalmers.group30.views.components.LeafletMap;
+import com.chalmers.group30.views.components.MapLibre;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import software.xdev.vaadin.maps.leaflet.flow.data.LCenter;
@@ -16,25 +17,29 @@ public class MapView extends VerticalLayout {
     public MapView() {
         setSpacing(false);
 
-        LeafletMap map = new LeafletMap();
-        map.setCenter(new LCenter(57.689798, 11.9736852));
-        map.setZoom(17);
-        map.setTileLayer(LTileLayer.DEFAULT_OPENSTREETMAP_TILE);
+        //LeafletMap map = new LeafletMap();
+        //map.setCenter(new LCenter(57.689798, 11.9736852));
+        //map.setZoom(17);
+        //map.setTileLayer(LTileLayer.DEFAULT_OPENSTREETMAP_TILE);
+        //map.setSizeFull();
+        //try{
+        //    map.setGeoJson(new ChalmersMapsAPI().geoJson());
+        //} catch(Exception e) {
+        //    // Failed to add GeoJSON
+        //}
+        //
+        //try {
+        //    Route r =  Route.fromJSON(new ChalmersMapsAPI().route(new Location(57.696484034673915, 11.975264592149706), new Location(57.700295142972465, 11.965737593691228)));
+        //    map.drawRoute(r);
+        //} catch (Exception e) {
+        //    // Failed!
+        //}
+        //
+        //
+        //add(map);
+
+        MapLibre map = new MapLibre();
         map.setSizeFull();
-        try{
-            map.setGeoJson(new ChalmersMapsAPI().geoJson());
-        } catch(Exception e) {
-            // Failed to add GeoJSON
-        }
-
-        try {
-            Route r =  Route.fromJSON(new ChalmersMapsAPI().route(new Location(57.696484034673915, 11.975264592149706), new Location(57.700295142972465, 11.965737593691228)));
-            map.drawRoute(r);
-        } catch (Exception e) {
-            // Failed!
-        }
-
-
         add(map);
 
         setSizeFull();
