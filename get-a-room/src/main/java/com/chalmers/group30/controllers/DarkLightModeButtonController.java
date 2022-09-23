383cd98c2f6a00f50ce79f83b37416c0120dd5a3
multicolorist <multicolorist@users.noreply.github.com>
2022-09-23 15:21:55 +0200

    Fix for Vaadin not updating html-tag when switching themes

--- get-a-room/src/main/java/com/chalmers/group30/controllers/DarkLightModeButtonController.java
+++ get-a-room/src/main/java/com/chalmers/group30/controllers/DarkLightModeButtonController.java
@@ -1,22 +1,17 @@
 package com.chalmers.group30.controllers;
 
-import com.chalmers.group30.models.RoomServiceInterface;
-import com.chalmers.group30.models.objects.Room;
 import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.dom.ThemeList;
 import com.vaadin.flow.spring.annotation.UIScope;
 import com.vaadin.flow.theme.lumo.Lumo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.List;
+import static com.chalmers.group30.views.utilities.JavascriptUtils.setDOMTagAttribute;
 
 /**
  * Controller for the darkmode<->lightmode button
@@ -46,9 +41,18 @@ public class DarkLightModeButtonController {
             // TODO: Fix this by changing the HTML attribute, likely pure JS as it doesn't exist as a component?
             // getElement() only ever seems to return <body>
             if (themeList.contains(Lumo.DARK)) {
+                setDOMTagAttribute("html", "theme", "light");
+                // Page page = UI.getCurrent().getPage();
+                // page.executeJs("document.documentElement.setAttribute(\"theme\", \"light\");");
+
                 themeList.remove(Lumo.DARK);
+                themeList.add(Lumo.LIGHT);
                 e.getSource().setIcon(lightModeIcon);
             } else {
+                setDOMTagAttribute("html", "theme", "dark");
+                // Page page = UI.getCurrent().getPage();
+                // page.executeJs("document.documentElement.setAttribute(\"theme\", \"dark\");");
+                themeList.remove(Lumo.LIGHT);
                 themeList.add(Lumo.DARK);
                 e.getSource().setIcon(darkModeIcon);
             }
