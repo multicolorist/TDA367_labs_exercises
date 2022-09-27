26959fcfaddb034cabfc4dc9f0f6b4c17e8bba61
multicolorist <multicolorist@users.noreply.github.com>
2022-09-27 18:51:32 +0200

    Styling to allow good inline behaviour of view components

--- get-a-room/src/main/java/com/chalmers/group30/views/components/buttons/BookButton.java
+++ get-a-room/src/main/java/com/chalmers/group30/views/components/buttons/BookButton.java
@@ -3,6 +3,7 @@ package com.chalmers.group30.views.components.buttons;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.theme.lumo.LumoUtility;
 
 import java.util.UUID;
 
@@ -14,9 +15,14 @@ public class BookButton extends Button implements ButtonHasUUID {
     Icon icon = new Icon(VaadinIcon.CALENDAR_CLOCK);
 
     public BookButton(UUID uuid) {
+        addClassNames(
+                LumoUtility.Display.INLINE_FLEX,
+                LumoUtility.BoxShadow.SMALL
+        );
         this.uuid = uuid;
         this.setIcon(icon);
         this.setText("Book in TimeEdit");
+
     }
 
     @Override
--- get-a-room/src/main/java/com/chalmers/group30/views/components/buttons/ShowOnMapButton.java
+++ get-a-room/src/main/java/com/chalmers/group30/views/components/buttons/ShowOnMapButton.java
@@ -3,6 +3,8 @@ package com.chalmers.group30.views.components.buttons;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
 import java.util.UUID;
 
 /**
@@ -13,6 +15,10 @@ public class ShowOnMapButton extends Button implements ButtonHasUUID {
     Icon icon = new Icon(VaadinIcon.MAP_MARKER);
 
     public ShowOnMapButton(UUID uuid) {
+        addClassNames(
+                LumoUtility.Display.INLINE_FLEX,
+                LumoUtility.BoxShadow.SMALL
+        );
         this.uuid = uuid;
         this.setIcon(icon);
         this.setText("Show on map");
