3e9634818402ff6b225a3cde4dee42846e7a7e51
GAsplund <gasplund2@gmail.com>
2022-09-29 21:33:50 +0200

    Remove old Leaflet component and its dependency

--- get-a-room/src/main/java/com/chalmers/group30/views/map/MapView.java
+++ get-a-room/src/main/java/com/chalmers/group30/views/map/MapView.java
@@ -11,34 +11,7 @@ public class MapView extends VerticalLayout {
     public MapView() {
         setSpacing(false);
 
-        //LeafletMap map = new LeafletMap();
-        //map.setCenter(new LCenter(57.689798, 11.9736852));
-        //map.setZoom(17);
-        //map.setTileLayer(LTileLayer.DEFAULT_OPENSTREETMAP_TILE);
-        //map.setSizeFull();
-        //try{
-        //    map.setGeoJson(new ChalmersMapsAPI().geoJson());
-        //} catch(Exception e) {
-        //    // Failed to add GeoJSON
-        //}
-        //
-        //try {
-        //    Route r =  Route.fromJSON(new ChalmersMapsAPI().route(new Location(57.696484034673915, 11.975264592149706), new Location(57.700295142972465, 11.965737593691228)));
-        //    map.drawRoute(r);
-        //} catch (Exception e) {
-        //    // Failed!
-        //}
-        //
-        //
-        //add(map);
-
         MapLibreContainer map = new MapLibreContainer();
-        //try {
-        //    map.addGeoJSON("buildings", new ChalmersMapsAPI().geoJson());
-        //    map.addExtrusionLayer("buildings_extrude", "buildings");
-        //} catch (Exception e) {
-        //    // Failed to add GeoJSON
-        //}
         map.setSizeFull();
         add(map);
 
