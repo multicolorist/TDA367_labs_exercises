e2aef63184b29fccf120bbf321ebee255bb7926b
GAsplund <gasplund2@gmail.com>
2022-10-06 20:17:46 +0200

    Add JavaDoc

--- get-a-room/src/main/java/com/chalmers/group30/views/components/GeolocationComponent.java
+++ get-a-room/src/main/java/com/chalmers/group30/views/components/GeolocationComponent.java
@@ -12,14 +12,21 @@ import java.util.logging.Logger;
 
 @Tag("geoloc-custom")
 @JsModule("./geolocation/geolocation.ts")
+/**
+ * A custom component that uses the browser's geolocation API to get the user's location
+ */
 public class GeolocationComponent extends Component {
 
-    Logger logger = Logger.getLogger(GeolocationComponent.class.getName());
+    private final Logger logger = Logger.getLogger(GeolocationComponent.class.getName());
 
     private Double latitude;
     private Double longitude;
     private Instant lastUpdated;
 
+    /**
+     * Gets the last latitude of the user
+     * @return The latitude of the user
+     */
     public Double getLatitude() {
         this.getElement().executeJs("return this.getLatitude();").then(Double.class, result -> {
             this.latitude = (result == null) ? Double.NaN : result;
@@ -27,6 +34,10 @@ public class GeolocationComponent extends Component {
         return this.latitude;
     }
 
+    /**
+     * Gets the last longitude of the user
+     * @return The longitude of the user
+     */
     public Double getLongitude() {
         this.getElement().executeJs("return this.getLongitude();").then(Double.class, result -> {
             this.longitude = (result == null) ? Double.NaN : result;
@@ -34,6 +45,9 @@ public class GeolocationComponent extends Component {
         return this.longitude;
     }
 
+    /**
+     * Attempts to get the user's location
+     */
     public void updateLocation() {
         this.getElement().executeJs("this.updateLocation();");
     }
