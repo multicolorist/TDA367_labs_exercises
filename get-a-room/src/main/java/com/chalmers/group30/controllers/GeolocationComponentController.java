732eb297384b9aa29a1dc150f9518b7d1c18623e
GAsplund <gasplund2@gmail.com>
2022-10-06 20:20:04 +0200

    Add controller JavaDoc

--- get-a-room/src/main/java/com/chalmers/group30/controllers/GeolocationComponentController.java
+++ get-a-room/src/main/java/com/chalmers/group30/controllers/GeolocationComponentController.java
@@ -3,6 +3,9 @@ package com.chalmers.group30.controllers;
 import com.chalmers.group30.models.objects.Location;
 import com.chalmers.group30.views.components.GeolocationComponent;
 
+/**
+ * Controller for the GeolocationComponent component
+ */
 public class GeolocationComponentController {
 
     private final GeolocationComponent geolocation;
@@ -11,6 +14,10 @@ public class GeolocationComponentController {
         this.geolocation = component;
     }
 
+    /**
+     * Gets the last tracked location of the user
+     * @return The last tracked location of the user
+     */
     public Location getLocation() {
         return new Location(geolocation.getLatitude(), geolocation.getLongitude());
     }
