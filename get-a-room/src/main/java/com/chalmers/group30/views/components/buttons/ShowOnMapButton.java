38670189331f40bdd295e1a1cf13dcbdc1c07fe1
multicolorist <multicolorist@users.noreply.github.com>
2022-10-07 17:16:15 +0200

    Update ShowOnMapButton to allow displaying on map

--- get-a-room/src/main/java/com/chalmers/group30/views/components/buttons/ShowOnMapButton.java
+++ get-a-room/src/main/java/com/chalmers/group30/views/components/buttons/ShowOnMapButton.java
@@ -1,5 +1,6 @@
 package com.chalmers.group30.views.components.buttons;
 
+import com.chalmers.group30.models.objects.Room;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.icon.Icon;
@@ -12,21 +13,21 @@ import java.util.UUID;
  * A button for showing a specific room on the map
  */
 public class ShowOnMapButton extends Button {
-    UUID uuid;
     Icon icon = new Icon(VaadinIcon.MAP_MARKER);
+    private Room room;
 
-    public ShowOnMapButton(UUID uuid) {
+    public ShowOnMapButton(Room room) {
+        this.room = room;
         addClassNames(
                 LumoUtility.Display.INLINE_FLEX,
                 LumoUtility.BoxShadow.SMALL
         );
         addThemeVariants(ButtonVariant.LUMO_SMALL);
-        this.uuid = uuid;
         this.setIcon(icon);
         this.setText("Show");
     }
 
-    public UUID getUUID() {
-        return this.uuid;
+    public Room getRoom() {
+        return this.room;
     }
 }
