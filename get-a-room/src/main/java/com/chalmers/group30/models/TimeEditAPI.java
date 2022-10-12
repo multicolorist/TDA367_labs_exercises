090a8eca61c98511b2f3266eab7b7d3864c08338
GAsplund <gasplund2@gmail.com>
2022-10-12 14:41:25 +0200

    Request Calendar in bulk

--- get-a-room/src/main/java/com/chalmers/group30/models/TimeEditAPI.java
+++ get-a-room/src/main/java/com/chalmers/group30/models/TimeEditAPI.java
@@ -6,6 +6,7 @@ import net.fortuna.ical4j.model.Calendar;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Utility for accessing the TimeEdit API
@@ -21,12 +22,30 @@ public class TimeEditAPI implements TimeEditAPIInterface{
     /**
      * Gets a schedule from TimeEdit for a given room and time period.
      * @param id The ID of the room
+     * @param start The start time of the period to get bookings for
+     * @param end The end time of the period to get bookings for
      * @return Calendar object representing all bookings for the room and given period
      * @throws IOException If the underlying API request failed for some reason
      * @throws ParserException If the underlying API request returned invalid or malformed data
      */
     public Calendar getSchedule(String id, LocalDateTime start, LocalDateTime end) throws IOException, ParserException {
-        String sURL = "https://cloud.timeedit.net/chalmers/web/public/ri.ical?sid=3&object=" + id + "&type=room";
-        return webRequests.readIcalendarFromUrl(sURL);
+        return getSchedule(List.of(id), start, end);
+    }
+
+    /**
+     *
+     * @param IDs A list of TimeEdit IDs for rooms
+     * @param start The start time of the period to get bookings for
+     * @param end The end time of the period to get bookings for
+     * @return Calendar object representing all bookings for the rooms and given period
+     * @throws IOException If the underlying API request failed for some reason
+     * @throws ParserException If the underlying API request returned invalid or malformed data
+     */
+    public Calendar getSchedule(List<String> IDs, LocalDateTime start, LocalDateTime end) throws IOException, ParserException {
+        StringBuilder sURL = new StringBuilder("https://cloud.timeedit.net/chalmers/web/public/ri.ical?sid=3");
+        for (String id : IDs) {
+            sURL.append("&object=").append(id).append("&type=room");
+        }
+        return webRequests.readIcalendarFromUrl(sURL.toString());
     }
 }
