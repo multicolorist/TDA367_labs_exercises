60ffef87c12194c7643f3d1919b0d15d2c3fdfdf
multicolorist <multicolorist@users.noreply.github.com>
2022-10-12 12:32:51 +0200

    Remove example sources, styles and resources

--- get-a-room/src/main/java/com/chalmers/group30/data/service/SampleAddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.chalmers.group30.data.service;
-
-import com.chalmers.group30.data.entity.SampleAddress;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SampleAddressRepository extends JpaRepository<SampleAddress, UUID> {
-
-}
