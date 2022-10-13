f7e1ad5323d7a8cd5f8430fa7fa045305a92d1a8
GAsplund <gasplund2@gmail.com>
2022-10-13 14:41:01 +0200

    Remove test for fetching room not in cache
    Functionality was removed from BookingService

--- get-a-room/src/test/java/com/chalmers/group30/models/BookingServiceTest.java
+++ get-a-room/src/test/java/com/chalmers/group30/models/BookingServiceTest.java
@@ -38,23 +38,4 @@ public class BookingServiceTest {
 
     }
 
-    @Test
-    public void getBookings_roomIsNotInCache() throws IOException, ParseException, ParserException {
-        Room mockRoom = new Room("Chalmers Test", 1, "Building A", "Floor 1", "Adress A", "chalmers:test", UUID.randomUUID(), new Location(0, 0), new Location(1, 1));
-        CacheUpdateProvider<Dictionary<Room, List<Booking>>> cacheUpdateProvider = mock(CacheUpdateProvider.class);
-
-        when(cacheUpdateProvider.getNewDataToCache()).thenReturn(new Hashtable<>());
-
-        BookingServiceInterface bookingService = new BookingService(cacheUpdateProvider);
-
-        bookingService.refreshBookingCache();
-
-        List<Booking> bookings = bookingService.getBookings(mockRoom);
-
-        assertEquals(1, bookings.size());
-
-
-
-    }
-
 }
